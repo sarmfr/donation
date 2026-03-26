0c1d1ed19d01f474771d951ff4692ec7de6917b0
sarmfr <kihungisamson@gmail.com>
2026-03-26 01:38:58 -0700

    Add Wasmer app config and runtime compatibility fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Wasmer's PHP runtime may miss mb_split even with mbstring polyfills present.
+if (! function_exists('mb_split')) {
+    function mb_split(string $pattern, string $string, int $limit = -1): array|false
+    {
+        $delimiter = '/';
+        $safePattern = str_replace($delimiter, '\\'.$delimiter, $pattern);
+
+        return preg_split($delimiter.$safePattern.$delimiter.'u', $string, $limit);
+    }
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
